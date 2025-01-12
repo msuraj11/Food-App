216ba56e22fdf6bb9a7755f7cafd39e459c5eb3c
msuraj11 <suraj.mamidi@gmail.com>
2025-01-12 18:51:12 +0100

    Added meal details page and share meals skelton.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,37 @@
-export default function MealSlugPage() {
-  return <h1>The any page.</h1>;
+import Image from 'next/image';
+import {notFound} from 'next/navigation';
+import classes from './page.module.css';
+import {getMeal} from '@/lib/meals';
+
+export default async function MealSlugPage({params}) {
+  const {mealSlug} = await params;
+  const meal = getMeal(mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.summary} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, '<br />')
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
